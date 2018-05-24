b2b96802ea64467b9ed8ef866bc76b64ea6df41d
Dave Longley <dlongley@digitalbazaar.com>
2018-05-24 13:54:00 -0400

    Add `use strict`.

--- schemas/bedrock-account.js
+++ schemas/bedrock-account.js
@@ -1,6 +1,8 @@
 /*!
- * Copyright (c) 2019 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2018 Digital Bazaar, Inc. All rights reserved.
  */
+'use strict';
+
 const bedrock = require('bedrock');
 
 const schema = {
