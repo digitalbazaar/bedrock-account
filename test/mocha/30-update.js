bb007006493bc9ffc4d9eb5236adafa0f939aa3b
Dave Longley <dlongley@digitalbazaar.com>
2023-01-23 17:09:29 -0500

    Add update transaction tests.

--- test/mocha/30-update.js
+++ test/mocha/30-update.js
@@ -226,6 +226,14 @@ describe('update', () => {
     account.email.should.equal(email);
     should.exist(meta['special-meta']);
     meta['special-meta'].should.deep.equal(specialMeta);
+
+    // ensure proxy record is present
+    const proxyRecord = await database.collections['account-email'].findOne(
+      {email: newAccount.email});
+    should.exist(proxyRecord);
+    proxyRecord.should.have.keys(['_id', 'accountId', 'email']);
+    proxyRecord.accountId.should.equal(newAccount.id);
+    proxyRecord.email.should.equal(newAccount.email);
   });
   it('should update account and meta', async () => {
     const email = 'f1224b54-2348-45dc-8fb4-32575ae1aac3@example.com';
@@ -262,6 +270,19 @@ describe('update', () => {
     account.email.should.equal('UPDATED.' + email);
     should.exist(meta['special-meta']);
     meta['special-meta'].should.deep.equal(specialMeta);
+
+    // ensure old proxy record is gone
+    const proxyRecord1 = await database.collections['account-email'].findOne(
+      {email: newAccount.email});
+    should.not.exist(proxyRecord1);
+
+    // ensure proxy record is present
+    const proxyRecord2 = await database.collections['account-email'].findOne(
+      {email: updatedAccount.email});
+    should.exist(proxyRecord2);
+    proxyRecord2.should.have.keys(['_id', 'accountId', 'email']);
+    proxyRecord2.accountId.should.equal(newAccount.id);
+    proxyRecord2.email.should.equal('UPDATED.' + email);
   });
   it('should not allow "id" operations', async () => {
     const email = 'af12fba9-02e9-4178-aadb-169e4c501cbd@example.com';
@@ -287,5 +308,275 @@ describe('update', () => {
     should.exist(error);
     error.name.should.equal('TypeError');
     error.message.should.include('"id" must equal "account.id".');
+  });
+
+  describe('transactions', () => {
+    it('should update email when txn failed before proxy ops', async () => {
+      const email = 'c534a3ae-aa06-4609-9473-cd60c1501d3c@example.com';
+      const newAccount = helpers.createAccount(email);
+      const newRecord = await brAccount.insert({account: newAccount});
+
+      // simulate failed transaction
+      await helpers.createFailedTransaction(
+        {accountId: newAccount.id, type: 'update'});
+
+      const updatedAccount = {...newRecord.account};
+      updatedAccount.email = 'UPDATED.' + email;
+      await brAccount.update({
+        account: updatedAccount,
+        sequence: 0
+      });
+      const updatedRecord = await database.collections.account.findOne(
+        {'account.id': newAccount.id});
+      should.exist(updatedRecord);
+      updatedRecord.should.have.keys('_id', 'account', 'meta');
+      const {account, meta} = updatedRecord;
+      meta.should.be.an('object');
+      should.exist(meta.created);
+      meta.created.should.be.a('number');
+      should.exist(meta.updated);
+      meta.updated.should.be.a('number');
+      meta.status.should.equal('active');
+      account.should.be.an('object');
+      account.id.should.equal(newAccount.id);
+      account.email.should.equal('UPDATED.' + email);
+
+      // ensure old proxy record is gone
+      const proxyRecord1 = await database.collections['account-email'].findOne(
+        {email: newAccount.email});
+      should.not.exist(proxyRecord1);
+
+      // ensure proxy record is present
+      const proxyRecord2 = await database.collections['account-email'].findOne(
+        {email: updatedAccount.email});
+      should.exist(proxyRecord2);
+      proxyRecord2.should.have.keys(['_id', 'accountId', 'email']);
+      proxyRecord2.accountId.should.equal(newAccount.id);
+      proxyRecord2.email.should.equal('UPDATED.' + email);
+    });
+    it('should update email when update txn w/proxy ops failed', async () => {
+      // this test simulates a previous update transaction that did not commit
+      // and hasn't been rolled back yet
+      const email = '9d559852-df9a-4760-8e8e-f539e6b932ef@example.com';
+      const newAccount = helpers.createAccount(email);
+      const newRecord = await brAccount.insert({account: newAccount});
+
+      // simulate failed transaction
+      await helpers.createFailedTransaction({
+        accountId: newAccount.id, type: 'update',
+        ops: [
+          {type: 'insert', email: 'failedchanged@example.com'},
+          {type: 'delete', email}
+        ]
+      });
+
+      const updatedAccount = {...newRecord.account};
+      updatedAccount.email = 'UPDATED.' + email;
+      await brAccount.update({
+        account: updatedAccount,
+        sequence: 0
+      });
+      const updatedRecord = await database.collections.account.findOne(
+        {'account.id': newAccount.id});
+      should.exist(updatedRecord);
+      updatedRecord.should.have.keys('_id', 'account', 'meta');
+      const {account, meta} = updatedRecord;
+      meta.should.be.an('object');
+      should.exist(meta.created);
+      meta.created.should.be.a('number');
+      should.exist(meta.updated);
+      meta.updated.should.be.a('number');
+      meta.status.should.equal('active');
+      account.should.be.an('object');
+      account.id.should.equal(newAccount.id);
+      account.email.should.equal('UPDATED.' + email);
+
+      // ensure old proxy record is gone
+      const proxyRecord1 = await database.collections['account-email'].findOne(
+        {email: newAccount.email});
+      should.not.exist(proxyRecord1);
+
+      // ensure proxy record is present
+      const proxyRecord2 = await database.collections['account-email'].findOne(
+        {email: updatedAccount.email});
+      should.exist(proxyRecord2);
+      proxyRecord2.should.have.keys(['_id', 'accountId', 'email']);
+      proxyRecord2.accountId.should.equal(newAccount.id);
+      proxyRecord2.email.should.equal('UPDATED.' + email);
+    });
+    it('should update with rolled back insert txn w/proxy op', async () => {
+      // this test simulates a stalled insert transaction that was rolled back
+      // by another process but still wrote an insert proxy op; that insert
+      // proxy op should be auto-rolled back and the update should succeed
+      const email = 'e5e8a18e-250c-4f3a-ace4-fe3f75bc0a86@example.com';
+      const newAccount = helpers.createAccount(email);
+      const newRecord = await brAccount.insert({account: newAccount});
+      const updatedAccount = {...newRecord.account};
+      updatedAccount.email = 'UPDATED.' + email;
+
+      // simulate failed transaction
+      const failedInsertId = 'acf8342e-09b8-4d2a-adaf-0c14c2a65086';
+      await helpers.createFailedTransaction({
+        accountId: failedInsertId,
+        type: 'insert',
+        ops: [
+          // this failed insert "blocks" the update
+          {type: 'insert', email: updatedAccount.email}
+        ],
+        skipAccountRecord: true
+      });
+
+      await brAccount.update({
+        account: updatedAccount,
+        sequence: 0
+      });
+      const updatedRecord = await database.collections.account.findOne(
+        {'account.id': newAccount.id});
+      should.exist(updatedRecord);
+      updatedRecord.should.have.keys('_id', 'account', 'meta');
+      const {account, meta} = updatedRecord;
+      meta.should.be.an('object');
+      should.exist(meta.created);
+      meta.created.should.be.a('number');
+      should.exist(meta.updated);
+      meta.updated.should.be.a('number');
+      meta.status.should.equal('active');
+      account.should.be.an('object');
+      account.id.should.equal(newAccount.id);
+      account.email.should.equal('UPDATED.' + email);
+
+      // ensure old proxy record is gone
+      const proxyRecord1 = await database.collections['account-email'].findOne(
+        {email: newAccount.email});
+      should.not.exist(proxyRecord1);
+
+      // ensure proxy record is present
+      const proxyRecord2 = await database.collections['account-email'].findOne(
+        {email: updatedAccount.email});
+      should.exist(proxyRecord2);
+      proxyRecord2.should.have.keys(['_id', 'accountId', 'email']);
+      proxyRecord2.accountId.should.equal(newAccount.id);
+      proxyRecord2.email.should.equal('UPDATED.' + email);
+    });
+    it('should update with rolled back insert txn w/proxy op', async () => {
+      // this test simulates a stalled update transaction that was rolled back
+      // by another process but still wrote an insert proxy op; that insert
+      // proxy op should be auto-rolled back and this update should pass
+      const email = '7ba5baf3-1713-4abe-afef-ba5ac095a046@example.com';
+      const newAccount = helpers.createAccount(email);
+      const newRecord = await brAccount.insert({account: newAccount});
+      const updatedAccount = {...newRecord.account};
+      updatedAccount.email = 'UPDATED.' + email;
+
+      // simulate failed transaction
+      const failedInsertId = '67b6786b-427f-4c83-8f84-66693ca6aaf4';
+      await helpers.createFailedTransaction({
+        accountId: failedInsertId,
+        type: 'insert',
+        ops: [
+          // this failed insert "blocks" the update
+          {type: 'insert', email: updatedAccount.email}
+        ],
+        skipAccountRecord: true
+      });
+
+      await brAccount.update({
+        account: updatedAccount,
+        sequence: 0
+      });
+      const updatedRecord = await database.collections.account.findOne(
+        {'account.id': newAccount.id});
+      should.exist(updatedRecord);
+      updatedRecord.should.have.keys('_id', 'account', 'meta');
+      const {account, meta} = updatedRecord;
+      meta.should.be.an('object');
+      should.exist(meta.created);
+      meta.created.should.be.a('number');
+      should.exist(meta.updated);
+      meta.updated.should.be.a('number');
+      meta.status.should.equal('active');
+      account.should.be.an('object');
+      account.id.should.equal(newAccount.id);
+      account.email.should.equal('UPDATED.' + email);
+
+      // ensure old proxy record is gone
+      const proxyRecord1 = await database.collections['account-email'].findOne(
+        {email: newAccount.email});
+      should.not.exist(proxyRecord1);
+
+      // ensure proxy record is present
+      const proxyRecord2 = await database.collections['account-email'].findOne(
+        {email: updatedAccount.email});
+      should.exist(proxyRecord2);
+      proxyRecord2.should.have.keys(['_id', 'accountId', 'email']);
+      proxyRecord2.accountId.should.equal(newAccount.id);
+      proxyRecord2.email.should.equal('UPDATED.' + email);
+    });
+    it(
+      'should throw duplicate error with rolled back update txn w/proxy op',
+      async () => {
+        // this test simulates a stalled update transaction that was rolled
+        // back by another process but still wrote a delete proxy op; that
+        // delete proxy op should be rolled back but the update should fail
+        // because of a duplicate error
+        const email = '8667deee-b068-4fb8-9399-d5f2985db9a4@example.com';
+        const newAccount = helpers.createAccount(email);
+        const newRecord = await brAccount.insert({account: newAccount});
+        const updatedAccount = {...newRecord.account};
+        updatedAccount.email = 'UPDATED.' + email;
+
+        // insert record that will fail to be updated that blocks the updated
+        // email
+        const failedUpdateAccount = helpers.createAccount(updatedAccount.email);
+        await brAccount.insert({account: failedUpdateAccount});
+
+        // simulate failed transaction
+        await helpers.createFailedTransaction({
+          accountId: failedUpdateAccount.id,
+          type: 'update',
+          ops: [
+            // this failed delete "blocks" the update
+            {type: 'delete', email: updatedAccount.email}
+          ]
+        });
+
+        let result;
+        let error;
+        try {
+          result = await brAccount.update({
+            account: updatedAccount,
+            sequence: 0
+          });
+        } catch(e) {
+          error = e;
+        }
+        should.not.exist(result);
+        should.exist(error);
+        error.name.should.equal('DuplicateError');
+        should.exist(error.details);
+        error.details.should.include.keys([
+          'recordId', 'account', 'uniqueField', 'uniqueValue'
+        ]);
+        error.details.recordId.should.equal(newAccount.id);
+        error.details.account.should.equal(newAccount.id);
+        error.details.uniqueField.should.equal('email');
+        error.details.uniqueValue.should.equal('UPDATED.' + email);
+
+        // ensure failed update account exists with no _txn
+        const blockingRecord = await database.collections.account.findOne(
+          {'account.id': failedUpdateAccount.id});
+        should.exist(blockingRecord);
+        blockingRecord.should.have.keys(['_id', 'account', 'meta']);
+        blockingRecord.account.id.should.equal(failedUpdateAccount.id);
+        blockingRecord.account.email.should.equal('UPDATED.' + email);
+
+        // ensure old proxy record is still present, but has no `_txn` field
+        const proxyRecord = await database.collections['account-email']
+          .findOne({email: failedUpdateAccount.email});
+        should.exist(proxyRecord);
+        proxyRecord.should.have.keys(['_id', 'accountId', 'email']);
+        proxyRecord.accountId.should.equal(failedUpdateAccount.id);
+        proxyRecord.email.should.equal('UPDATED.' + email);
+      });
   });
 });
